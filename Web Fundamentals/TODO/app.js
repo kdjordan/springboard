8f6baaa414f24ea53c5781f0669264ab0caabe8c
kdjordan <k.dean.jordan@gmail.com>
2022-02-08 16:16:20 -0800

    todo section 2 done

--- Web Fundamentals/TODO/app.js
+++ Web Fundamentals/TODO/app.js
@@ -2,14 +2,20 @@ const theInput = document.querySelector('#input')
 const theUl = document.querySelector('ul')
 const theForm = document.querySelector('form');
 
-let newTodo = null
-let todoToDelete = false
-
-const completedTodo = '<span class="todo-list__delete">X</span>'
+const savedTodos = window.localStorage
 
 
-
-
+if(savedTodos.length >= 1) {
+    console.log('running')
+    for (const key in localStorage) {
+        // Skip built-in properties like length, setItem, etc.
+        // code snippet taken from https://attacomsian.com/blog/javascript-iterate-over-local-storage-keys
+        if (localStorage.hasOwnProperty(key)) {
+            addTodo(key)
+        }
+    }
+    
+}
 theForm.addEventListener('submit', function(e) {
     e.preventDefault()
     newTodo = e.target.children[0].value
@@ -17,18 +23,17 @@ theForm.addEventListener('submit', function(e) {
         alert('You cant have an empty TODO !')
     } else {
         theForm.reset()
-        console.log('submitting',newTodo)
         addTodo(newTodo)
     }
 })
 
 theUl.addEventListener('click', function(e) {
-    // e.target.remove()
-    // if(e.target.)
-    // e.preventDefault()
-    // console.log(e.target.nodeName)
+
     if(e.target.nodeName == 'SPAN' && e.target.previousSibling.classList.contains('completed')) {
-        e.target.previousSibling.remove()
+        console.log(e.target.previousSibling.innerText)
+        localStorage.removeItem(e.target.previousSibling.innerText)
+        console.log(localStorage)
+        e.target.previousSibling.remove(e.target.previousSibling.innerText)
         e.target.remove()
     }
 
@@ -36,31 +41,22 @@ theUl.addEventListener('click', function(e) {
         if(e.target.classList.contains('completed')) {
             e.target.classList.remove('completed')
             e.target.nextSibling.remove()
-            // console.log(removeSpan.nextSibling)
-            // console.log(e.target.previousSibling)
         } else {
             e.target.classList.add('completed')
-            const newSpan = document.createElement('span')
-            newSpan.innerHTML = 'X'
-            // console.log(newSpan)
-            console.log(e)
             e.target.insertAdjacentHTML('afterend', '<span>X</span>')
         }
 
     }
-    // console.log(e.target.classList.contains('completed'))
-    // if(e.target.nodeName == 'LI' && e.target.classList.contains('completed')) {
-        
-    // }
-
-    
-    // todoCompleted(e.target)
 })
 
 function addTodo(todo) {
+    console.log('thetodo', todo)
     const newLi = document.createElement('div')
     newLi.innerHTML = `<li>${todo}</li>`
     theUl.appendChild(newLi)
+    localStorage.setItem(`${todo}`, `${todo}`)
+    console.log('added', localStorage)
+
 }
 
 function todoCompleted(e) {
