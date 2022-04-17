f9fefe6185138efa80a0bd8ab1da04a1217a039b
kdjordan <k.dean.jordan@gmail.com>
2022-04-17 12:00:39 -0700

    added comments

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -5,21 +5,25 @@ app = Flask(__name__)
 
 
 @app.route('/add')
+"""Get query string params and add values together"""
 def add():
     return f"{operations.add(int(request.args['a']), int(request.args['b']))}"
 
 
 @app.route('/sub')
+"""Get query string params and subtract values"""
 def sub():
     return f"{operations.sub(int(request.args['a']), int(request.args['b']))}"
 
 
 @app.route('/mult')
+"""Get query string params and multiply values"""
 def mult():
     return f"{operations.mult(int(request.args['a']), int(request.args['b']))}"
 
 
 @app.route('/div')
+"""Get query string params and divide values"""
 def div():
     return f"{operations.div(int(request.args['a']), int(request.args['b']))}"
 
