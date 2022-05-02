01d74e1285af21eebb9052d28ab89e97ef547d0a
kdjordan <k.dean.jordan@gmail.com>
2022-05-02 13:19:50 -0700

    added flask-blogly 23.1 ex

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from models import db, connect_db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///music'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -1,1 +1,9 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    
