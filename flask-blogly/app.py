dc05db38b3c2487b250aa57ebdaf0f7dacab60df
kdjordan <k.dean.jordan@gmail.com>
2022-05-03 10:48:50 -0700

    base markup done

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,12 +1,28 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///music'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
+# db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('adduser.html', title='Add User', heading='Create a User')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    # get form data and make into User 
+    user = User(first_name=request.form['first_name'], last_name=request.form['last_name'])
+    # insert into DB
+    db.session.add(user)
+    db.session.commit()
+    return render_template('base.html')
+
+
+
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -7,3 +7,27 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
     
+
+# MODELS
+class User(db.Model):
+    __tablename__ = 'users'
+
+    def __repr__(self):
+        u = self
+        return f'<User id={u.id}, First name={u.first_name}, Last Name={u.last_name}, Avatar={u.avatar}'
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+
+    first_name = db.Column(db.String(20),
+                    nullable=False,
+                    unique=False)
+
+    last_name = db.Column(db.String(20),
+                    nullable=False,
+                    unique=False)
+    
+    avatar = db.Column(db.String(),
+                    nullable=False,
+                    unique=False)
