520187deaacc3f5e6cb6699a7c7424f580d0daa7
kdjordan <k.dean.jordan@gmail.com>
2022-05-03 16:38:12 -0700

    routes defined

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -9,20 +9,48 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-# db.create_all()
+db.create_all()
 
 @app.route('/')
 def home():
-    return render_template('adduser.html', title='Add User', heading='Create a User')
+    return redirect('/users')
 
-@app.route('/add_user', methods=['POST'])
+@app.route('/users')
+def get_all_users():
+    users = User.query.all()
+    
+    return render_template('allusers.html', title='All Users', heading='Users', users=users)
+
+@app.route('/users/new', methods=['POST', 'GET'])
 def add_user():
-    # get form data and make into User 
-    user = User(first_name=request.form['first_name'], last_name=request.form['last_name'])
-    # insert into DB
-    db.session.add(user)
-    db.session.commit()
-    return render_template('base.html')
+    if request.method == 'POST':
+        # get form data and make into User 
+        user = User(first_name=request.form['first_name'], 
+                last_name=request.form['last_name'], 
+                avatar=request.form['avatar'])
+        # insert into DB
+        db.session.add(user)
+        db.session.commit()
+        return render_template('base.html')
+    if request.method == 'GET':
+        return render_template('adduser.html', title='Add User', heading='Create a User')
+
+
+@app.route('/users/<:id>')
+def get_user(id):
+    print('gettig id ', id)
+    return redirect('/users')
+
+@app.route('/users/<:id>/edit')
+def edit_user(id):
+    print('editing ', id)
+    return redirect('/users')
+
+@app.route('/users/<:id>/delete')
+def delete_user(id):
+    print('deleting ' id)
+    return redirect('/users')
+
 
 
 
