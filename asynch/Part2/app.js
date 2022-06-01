3cc0fd1b88a32a000c0efc57a53b0ef6abda7428
kdjordan <k.dean.jordan@gmail.com>
2022-06-01 10:14:52 -0700

    part 2 double card receipt working

--- asynch/Part2/app.js
+++ asynch/Part2/app.js
@@ -7,11 +7,25 @@ window.onload = () => {
 
 async function getDeck() {
     let deck = await axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    let card = await axios.get(`http://deckofcardsapi.com/api/deck/${deck.data.deck_id}/draw/?count=1`)
-    const { cards } = card.data
-    console.log(`The card is the ${cards[0].value} of ${cards[0].suit}`)
+    let card1 = getCard(deck.data.deck_id)
+    let card2 = getCard(deck.data.deck_id)
+
+    let allCards = await Promise.all([card1, card2])
+    printCards(allCards)
 }
 
 getCardButton.addEventListener('click', ()=> {
     console.log('getting card')
 })
+
+
+async function getCard(deck_id) {
+    let card = await axios.get(`http://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
+    return card.data
+}
+
+function printCards(cards) {
+    for (card of cards) {
+        console.log(`The card is the ${card.cards[0].value} of ${card.cards[0].suit}`)
+    }
+}
