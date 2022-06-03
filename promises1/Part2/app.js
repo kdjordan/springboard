4f62783ca34eabccff5b360e45bcb8824d7fec64
kdjordan <k.dean.jordan@gmail.com>
2022-06-02 17:48:20 -0700

    Part2 done

--- promises1/Part2/app.js
+++ promises1/Part2/app.js
@@ -39,19 +39,12 @@ window.onload = () => {
     .catch(e => console.log(e))
   }
 
-// get a deck id from API
-async function getDeck() {
-    let deck = await axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    let card1 = getCard(deck.data.deck_id)
-    let card2 = getCard(deck.data.deck_id)
-
-    let allCards = await Promise.all([card1, card2])
-    printCards(allCards)
-}
-
-async function getCard(deck_id) {
-    let card = await axios.get(`http://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
-    return card.data
+function getCard(deck_id) {
+    axios.get(`http://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
+    .then(res => {
+        console.log('res', res)
+        return card.data
+    })
 }
 
 function printCards(cards) {
@@ -61,18 +54,18 @@ function printCards(cards) {
 }
 
 
-async function getDisplayDeck() {
-    let res = await axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    displayDeck_id = res.data.deck_id
+function getDisplayDeck() {
+    axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    .then((res) => {
+        displayDeck_id = res.data.deck_id
+    })
 }
 
 
 getCardButton.addEventListener('click', ()=> {
-    
     axios.get(`http://deckofcardsapi.com/api/deck/${displayDeck_id}/draw/?count=1`)
     .then(res => {
         addCardImage(res.data.cards[0].image)
-
     })
 })
 
