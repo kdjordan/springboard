c65ea2401024dbcba3cf0e311e7859e16cfbbfe0
kdjordan <k.dean.jordan@gmail.com>
2022-06-05 08:46:25 -0700

    jest tests passing

--- node-markov/markov.js
+++ node-markov/markov.js
@@ -17,13 +17,42 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chain = new Map()
+    for(let i=0; i<this.words.length; i++) {
+      let nextWord = this.words[i+1] || null
+      let word = this.words[i]
+      if(chain.has(word)) {
+        chain.get(word).push(nextWord)
+      } else {
+        chain.set(word, [nextWord])
+      }
+
+    }
+    this.chain = chain
   }
-
-
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    const keys = Array.from(this.chain.keys())
+    let key = MarkovMachine.makeRandom(keys)
+    let returnArr = []
+    
+    while(returnArr.length < numWords &&  key !== null) {
+      returnArr.push(key)
+      key = MarkovMachine.makeRandom(keys)
+    }
+    console.log(returnArr)
+    return returnArr.join(' ')
+  }
+
+static makeRandom(array) {
+    return array[Math.floor(Math.random() * array.length)]
   }
 }
+// let mm = new MarkovMachine(`the cat in the hat`);
+
+// console.log(mm.makeText(numWords = 10))
+// mm.makeText(numWords = 5);
+
+
+module.exports = {MarkovMachine}
---
+++ node-markov/markov.test.js
@@ -0,0 +1,45 @@
+const { MarkovMachine } = require('./markov')
+
+let mm
+let output
+
+describe('test mm here', ()=> {
+    beforeEach(()=> {
+        mm = new MarkovMachine('the test input')
+        keys = Array.from(mm.chain.keys())
+        output = mm.makeText(numWords=10)
+        
+    })
+    // afterEach(()=> {
+    //     output = []
+    //     keys = []
+    //     mm = null
+    // })
+    test('first test', ()=> {
+        expect(mm).not.toBeUndefined()
+        expect(keys).toBeInstanceOf(Array)
+    })
+
+    test('check length of words on input', ()=> {
+        mm5 = new MarkovMachine('the cat in the hat')
+        expect(mm5.words.length).toEqual(5)
+        mm10 = new MarkovMachine('one two three four five six seven eight nine ten')
+        expect(mm10.words.length).toEqual(10)
+    })
+
+    test('check chain type ', ()=> {
+        expect(mm.chain).toBeInstanceOf(Map)
+    })
+
+    test('check output ', ()=> {
+        mmTest = new MarkovMachine('the cat in the hat')
+        let out = mmTest.makeText(numWords = 10)
+        expect(out).toContain('cat')
+        expect(out).toContain('the')
+        expect(out).toContain('in')
+        expect(out).toContain('the')
+        expect(out).toContain('hat')
+    })
+
+
+})
