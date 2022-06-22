c69dfc807531033c3c47c756b5429ac727401ed1
kdjordan <k.dean.jordan@gmail.com>
2022-06-21 18:10:45 -0700

    added slugify to comapnies post route

--- express-biztime/routes/companies.js
+++ express-biztime/routes/companies.js
@@ -43,6 +43,7 @@ router.put('/:code', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
     try {
         let { code, name, description } = req.body
+        code = slugify(code)
         const results = await db.query('INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description', [code, name, description])
         return res.status(201).json({company: results.rows[0]})
     } catch(e) {
