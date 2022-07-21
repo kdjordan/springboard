06327153b38844c0c3089ed0af95f1e60e9490e8
kdjordan <k.dean.jordan@gmail.com>
2022-07-20 18:53:38 -0700

    part 3 complete

---
+++ react-jsx-intro/part-3/App.js
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div>
+            <Person name='John' age={18} hobbies={['skiing', 'runnnig', 'coding']}/>
+            <Person name='Rapunzel' age={4} hobbies={['baking', 'reading', 'coding']}/>
+        </div>
+    )
+}
---
+++ react-jsx-intro/part-3/Person.js
@@ -0,0 +1,16 @@
+const Person = (props) => {
+    let mssg = props.age >= 18 ? 'Please go vote !' : 'You must be 18'
+    let name = props.name.length > 6 ? props.name.slice(0,5) : props.name
+    return (
+        <div>
+            <h1>Learn Some Information about this person !</h1>
+            <p>This {name} is {props.age} years old.</p>
+            <h3><i>{mssg}</i></h3>
+            <h3>Hobbies</h3>
+            <ul>
+                {/* {props.hobbies.map(el => <li>el</li>) } */}
+                {props.hobbies.map((h, i) => <li key={i}>{h}</li>)}
+            </ul>
+        </div>
+    )
+}
