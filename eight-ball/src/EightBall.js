21043987e12f571c4b89d02c69aa0686fe9a8ee1
kdjordan <k.dean.jordan@gmail.com>
2022-07-22 08:54:14 -0700

    color state working

--- eight-ball/src/EightBall.js
+++ eight-ball/src/EightBall.js
@@ -4,21 +4,40 @@ import data from './EightBallData'
 
 const EightBall = () => {
     const [mssg, setMssg] = useState('Think of a question...')
+    const [backgroundColor, setColor] = useState('black')
+    const [fontColor, setFontColor] = useState('red')
+
     let index = Math.floor(Math.random() * data.length)
     let newMssg = data[index]['msg']
+    let color = data[index]['color']
+
+    function doUpdate() {
+        setMssg(newMssg)
+        setColor(color)
+        setFontColor('white')
+    }
+
+    function reset() {
+        setMssg('Think of a question...')
+        setColor('black')
+        setFontColor('red')
+    }
 
     return (
         <div className="EightBall">
-            <div className="EightBall-circle">
-                <div className="EightBall-text">
+            <div className="EightBall-circle" 
+                style={{backgroundColor: backgroundColor, borderColor: fontColor}}
+            >
+                <div className="EightBall-text" style={{color: fontColor}}>
                 {mssg}
                 </div>
             </div>
             <div>
-                <button id="go-btn" onClick={() => setMssg(newMssg)}>SOLUTION</button>
+                {/* <button id="go-btn" onClick={() => setMssg(newMssg)}>SOLUTION</button> */}
+                <button id="go-btn" onClick={doUpdate}>SOLUTION</button>
             </div>
             <div>
-                <button id="go-btn" onClick={() => setMssg('Think of a question...')}>RESET</button>
+                <button id="go-btn" onClick={reset}>RESET</button>
             </div>
         </div>
     )
