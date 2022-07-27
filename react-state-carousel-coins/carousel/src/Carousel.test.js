284c38699b9b9550abb707beaf16d1d3aa21d841
kdjordan <k.dean.jordan@gmail.com>
2022-07-26 18:42:45 -0700

    left arrow tests ✅

--- react-state-carousel-coins/carousel/src/Carousel.test.js
+++ react-state-carousel-coins/carousel/src/Carousel.test.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import Carousel from "./Carousel";
 
 //smoke test
 it("renders without crashing", function() {
-  render(<Carousel />);
+    render(<Carousel />);
 });
 
 // snapshot test
@@ -12,3 +12,33 @@ it("matches snapshot", function() {
     const {asFragment} = render(<Carousel />);
     expect(asFragment()).toMatchSnapshot();
   });
+
+   
+it('left arrow tests', ()=> {
+    const {queryByAltText, getByTestId} = render(<Carousel />)
+
+    //only one alttest should show
+    expect(queryByAltText('Photo by Richard Pasquarella on Unsplash')).toBeInTheDocument()
+    expect(queryByAltText('Photo by Pratik Patel on Unsplash')).not.toBeInTheDocument() 
+
+    //click left arrrow should not be since FAIL
+    const lftBtn = getByTestId('left-arrow')
+    fireEvent.click(lftBtn)
+    expect(queryByAltText('Photo by Josh Post on Unsplash')).not.toBeInTheDocument()
+
+})
+
+it('right arrow tests', ()=> {
+    const {queryByTestId, queryByAltText, getByTestId} = render(<Carousel />)
+
+    //only one alttest should show
+    expect(queryByAltText('Photo by Richard Pasquarella on Unsplash')).toBeInTheDocument()
+    expect(queryByAltText('Photo by Pratik Patel on Unsplash')).not.toBeInTheDocument() 
+
+    //click left arrrow should not be since FAIL
+    const lftBtn = getByTestId('left-arrow')
+    fireEvent.click(lftBtn)
+    expect(queryByAltText('Photo by Josh Post on Unsplash')).not.toBeInTheDocument()
+
+})
+
