2cc36c164ef547b4cacb24ba85e5cf6ef9bcc4f6
kdjordan <k.dean.jordan@gmail.com>
2022-07-26 17:57:32 -0700

    parts 1 & 2 ✅

--- react-state-carousel-coins/carousel/src/Card.test.js
+++ react-state-carousel-coins/carousel/src/Card.test.js
@@ -2,6 +2,13 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Card from "./Card";
 
+//smoke test
 it("renders without crashing", function() {
   render(<Card />);
 });
+
+// snapshot test
+it("matches snapshot", function() {
+    const {asFragment} = render(<Card />);
+    expect(asFragment()).toMatchSnapshot();
+  });
--- react-state-carousel-coins/carousel/src/Carousel.test.js
+++ react-state-carousel-coins/carousel/src/Carousel.test.js
@@ -2,6 +2,13 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+//smoke test
 it("renders without crashing", function() {
   render(<Carousel />);
 });
+
+// snapshot test
+it("matches snapshot", function() {
+    const {asFragment} = render(<Carousel />);
+    expect(asFragment()).toMatchSnapshot();
+  });
