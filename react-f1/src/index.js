97ac784c2a60523c4aa981d6a0c636a903255498
kdjordan <k.dean.jordan@gmail.com>
2022-08-04 14:25:32 -0700

    fix react version

--- react-f1/src/index.js
+++ react-f1/src/index.js
@@ -1,8 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -17,9 +15,3 @@ root.render(
   </StrictMode>
 );
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
--- react-forms398-p2/src/App.test.js
+++ react-forms398-p2/src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, fireEvent, wait } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', function() {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", function() {
+  render(<App />);
 });
+
+it("matches snapshot", function() {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
