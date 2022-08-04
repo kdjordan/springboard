3e9c7e10a292ba1778171a82f3b2d254a9693761
kdjordan <k.dean.jordan@gmail.com>
2022-08-03 19:10:21 -0700

    testing struggles now

--- react-forms398-p1/src/BoxList.js
+++ react-forms398-p1/src/BoxList.js
@@ -2,17 +2,17 @@ import React from 'react'
 import Box from './Box'
 import './BoxList.css'
 
-function BoxList(props) {
-    console.log('gettint ', props)
+function BoxList({data, removeBox}) {
+    console.log('gettint ', data)
     const deleteBox = (e, boxId) => {
-        props.removeBox(() => e, boxId)
+        removeBox(() => e, boxId)
     }
 
  return (
     <div className='BoxList'>
         <h2>Currrent Boxes</h2>
         <div className="BoxList-container">
-            { props.data.map(box =>  {
+            { data.map(box =>  {
                 return (
                     <Box id={box.id} height={box.height} width={box.width} color={box.color} deleteBox={deleteBox}/>
                 )
--- react-forms398-p1/src/NewBoxForm.test.js
+++ react-forms398-p1/src/NewBoxForm.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BoxList from './BoxList';
+import NewBoxForm from './NewBoxForm';
 
 it('renders without crashing', function() {
   const div = document.createElement('div');
-  ReactDOM.render(<BoxList />, div);
+  ReactDOM.render(<NewBoxForm />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
