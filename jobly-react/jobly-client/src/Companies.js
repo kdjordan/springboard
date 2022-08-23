100867c71b6264856d68901aded43b2e711c8709
kdjordan <k.dean.jordan@gmail.com>
2022-08-22 21:10:31 -0700

    css work

--- jobly-react/jobly-client/src/Companies.js
+++ jobly-react/jobly-client/src/Companies.js
@@ -1,9 +1,20 @@
 import React from "react";
+import './Companies.css'
+
 
 export default function Companies() {
     return (
-        <div>
-            <h1>Companies</h1>
+        <div className="Companies">
+            <form>
+                <input 
+                type="text" 
+                name="" 
+                id="" 
+                placeholder="Enter search term..."
+            />
+            <button type="submit">Submit</button>
+
+            </form>
         </div>
     )
 }
