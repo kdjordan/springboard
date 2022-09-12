cade5d4aff5ded466140a37d58236513d075aded
kdjordan <k.dean.jordan@gmail.com>
2022-09-11 17:52:41 -0700

    added utility classes for LL

--- DSA_47/47_4/linked-list.js
+++ DSA_47/47_4/linked-list.js
@@ -18,27 +18,61 @@ class LinkedList {
     for (let val of vals) this.push(val);
   }
 
+  /** utility class to return a node at an index => DRY as this code will be used in multiple places **/
+  _get(idx) {
+    let cur = this.head
+    let count = 0
+    while (cur !== null && count != idx) {
+      count++
+      cur = cur.next
+    }
+    return cur
+  }
+  /** utility class to check if index is in range => DRY as this code will be used in multiple places **/
+  _checkIdx(idx) {
+    if (idx >= this.length || idx < 0) {
+      throw new Error ('Index out of range')
+    }
+  }
+
   /** push(val): add new value to end of list. */
 
   push(val) {
     let newNode = new Node(val)
     if (this.length === 0) {
       this.head = newNode
+      this.tail = this.head
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
     }
     this.length++
-    this.tail = newNode
-    return newNode
+    
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
+    let newNode = newNode(val)
 
+    //handle adding first node
+    if (this.head === null) {
+      this.head = newNode
+    } else {
+      newNode.next = this.head
+      this.head = newNode
+    }
+
+    //if only one node present 
+    if (this.length === 0) this.tail = this.head
+
+    this.length++
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+   
 
   }
 
@@ -51,7 +85,10 @@ class LinkedList {
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-
+    //check for boundary error first = will throw Error if idx out of range
+    this._checkIdx(idx)
+      
+    console.log('made it ', idx)
   }
 
   /** setAt(idx, val): set val at idx to val */
@@ -69,6 +106,24 @@ class LinkedList {
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
+    //check for boundary error
+    if (idx >= this.length || idx < 0) {
+      throw new Error ('Index out of range')
+    }
+
+    //handle index = 0
+    if (idx === 0) {
+      let val = this.head.val
+      this.head = this.head.next
+      this.length--
+      return val
+    }
+
+    //handle removing last node
+    if (idx === this.length - 1) {
+      let val = this.tail.val
+      return this.tail
+    }
 
   }
 
@@ -78,5 +133,9 @@ class LinkedList {
     
   }
 }
+
+let theList = new LinkedList([1,2,3])
+console.log(theList)
+// theList.pop()
 
 module.exports = LinkedList;
