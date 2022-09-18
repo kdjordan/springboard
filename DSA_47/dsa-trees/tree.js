2d687cb6481acb33ec758037c3f195046df388d2
kdjordan <k.dean.jordan@gmail.com>
2022-09-18 12:36:02 -0700

    first tree ex done with tests passing

--- DSA_47/dsa-trees/tree.js
+++ DSA_47/dsa-trees/tree.js
@@ -5,6 +5,49 @@ class TreeNode {
     this.val = val;
     this.children = children;
   }
+
+  sumValues() {
+    const theStack = [this]
+    let total = 0
+    while (theStack.length) {
+      const curr = theStack.pop()
+      total += curr.val
+      for (let child of curr.children) {
+        theStack.push(child)
+      }
+    }
+    return total
+  }
+
+  countEvens() {
+    const theStack = [this]
+    let count = 0
+    while (theStack.length) {
+      const curr = theStack.pop()
+      if (curr.val % 2 === 0) {
+        count++
+      }
+      for (let child of curr.children) {
+        theStack.push(child)
+      }
+    }
+    return count
+  }
+
+  numGreater(lowerBound) {
+    const theStack = [this]
+    let count = 0
+    while (theStack.length) {
+      const curr = theStack.pop()
+      if (curr.val > lowerBound) {
+        count++
+      }
+      for (let child of curr.children) {
+        theStack.push(child)
+      }
+    }
+    return count
+  }
 }
 
 class Tree {
@@ -15,20 +58,31 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if(this.root) {
+      return this.root.sumValues()
+    } else {
+      return 0
+    }
   }
-
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    if (this.root) {
+      return this.root.countEvens()
+    } else {
+      return 0
+    }
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    if (this.root) {
+      return this.root.numGreater(lowerBound)
+    } else {
+      return 0
+    }
   }
 }
 
