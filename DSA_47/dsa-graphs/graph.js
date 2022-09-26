80329ca20387807ca997c5046ad9c74a2d4bf618
kdjordan <k.dean.jordan@gmail.com>
2022-09-26 12:56:21 -0700

    DFS done

--- DSA_47/dsa-graphs/graph.js
+++ DSA_47/dsa-graphs/graph.js
@@ -27,16 +27,46 @@ class Graph {
   }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    if (!v1.adjacent.has(v2)) v1.adjacent.add(v2)
+    if (!v2.adjacent.has(v1)) v2.adjacent.add(v1)
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    if (v1.adjacent.has(v2)) v1.adjacent.delete(v2)
+    if (v2.adjacent.has(v1)) v2.adjacent.delete(v1)
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    if (this.nodes.has(vertex)) {
+      this.nodes.delete(vertex)
+    }
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    if(!this.nodes.has(start)) return
+    let arr = []
+    let stack = [start]
+    let seen = new Set()
+    seen.add(start)
+
+    while (stack.length) {
+      let curr = stack.pop()
+      arr.push(curr.value)
+
+      for (let node of curr.adjacent) {
+        if (!seen.has(node)) {
+          stack.push(node)
+          seen.add(node)
+        }
+      }
+    }
+    return arr
+      
+  }
 
   // this function returns an array of Node values using BFS
   breadthFirstSearch(start) {}
--- DSA_47/dsa-graphs/graph.test.js
+++ DSA_47/dsa-graphs/graph.test.js
@@ -1,38 +1,38 @@
 const { Graph, Node } = require("./graph");
 
-describe("addVertex", function() {
-  it("should add a key in the adjacency", function() {
-    let graph = new Graph();
-    let a = new Node("A");
-    let b = new Node("B");
-    let c = new Node("C");
-    expect(graph.nodes.has(a)).toBe(false);
-    expect(graph.nodes.has(b)).toBe(false);
-    expect(graph.nodes.has(c)).toBe(false);
-    graph.addVertex(a);
-    graph.addVertex(b);
-    graph.addVertex(c);
-    expect(graph.nodes.has(a)).toBe(true);
-    expect(graph.nodes.has(b)).toBe(true);
-    expect(graph.nodes.has(c)).toBe(true);
-  });
-});
+// describe("addVertex", function() {
+//   it("should add a key in the adjacency", function() {
+//     let graph = new Graph();
+//     let a = new Node("A");
+//     let b = new Node("B");
+//     let c = new Node("C");
+//     expect(graph.nodes.has(a)).toBe(false);
+//     expect(graph.nodes.has(b)).toBe(false);
+//     expect(graph.nodes.has(c)).toBe(false);
+//     graph.addVertex(a);
+//     graph.addVertex(b);
+//     graph.addVertex(c);
+//     expect(graph.nodes.has(a)).toBe(true);
+//     expect(graph.nodes.has(b)).toBe(true);
+//     expect(graph.nodes.has(c)).toBe(true);
+//   });
+// });
 
-describe("addVertices", function() {
-  it("should add multiple keys in the adjacency", function() {
-    let graph = new Graph();
-    let a = new Node("A");
-    let b = new Node("B");
-    let c = new Node("C");
-    expect(graph.nodes.has(a)).toBe(false);
-    expect(graph.nodes.has(b)).toBe(false);
-    expect(graph.nodes.has(c)).toBe(false);
-    graph.addVertices([a, b, c]);
-    expect(graph.nodes.has(a)).toBe(true);
-    expect(graph.nodes.has(b)).toBe(true);
-    expect(graph.nodes.has(c)).toBe(true);
-  });
-});
+// describe("addVertices", function() {
+//   it("should add multiple keys in the adjacency", function() {
+//     let graph = new Graph();
+//     let a = new Node("A");
+//     let b = new Node("B");
+//     let c = new Node("C");
+//     expect(graph.nodes.has(a)).toBe(false);
+//     expect(graph.nodes.has(b)).toBe(false);
+//     expect(graph.nodes.has(c)).toBe(false);
+//     graph.addVertices([a, b, c]);
+//     expect(graph.nodes.has(a)).toBe(true);
+//     expect(graph.nodes.has(b)).toBe(true);
+//     expect(graph.nodes.has(c)).toBe(true);
+//   });
+// });
 
 // describe("addEdge", function() {
 //   it("should add the appropriate edges to the adjacency list", function() {
@@ -97,54 +97,54 @@ describe("addVertices", function() {
 //   });
 // });
 
-// describe("DFS", function() {
-//   it("return an array of the nodes searched using DFS", function() {
-//     let graph = new Graph();
-//     let S = new Node("S");
-//     let P = new Node("P");
-//     let U = new Node("U");
-//     let X = new Node("X");
-//     let Q = new Node("Q");
-//     let Y = new Node("Y");
-//     let V = new Node("V");
-//     let R = new Node("R");
-//     let W = new Node("W");
-//     let T = new Node("T");
+describe("DFS", function() {
+  it("return an array of the nodes searched using DFS", function() {
+    let graph = new Graph();
+    let S = new Node("S");
+    let P = new Node("P");
+    let U = new Node("U");
+    let X = new Node("X");
+    let Q = new Node("Q");
+    let Y = new Node("Y");
+    let V = new Node("V");
+    let R = new Node("R");
+    let W = new Node("W");
+    let T = new Node("T");
 
-//     graph.addVertices([S, P, U, X, Q, Y, V, R, W, T]);
+    graph.addVertices([S, P, U, X, Q, Y, V, R, W, T]);
 
-//     graph.addEdge(S, P);
-//     graph.addEdge(S, U);
+    graph.addEdge(S, P);
+    graph.addEdge(S, U);
 
-//     graph.addEdge(P, X);
-//     graph.addEdge(U, X);
+    graph.addEdge(P, X);
+    graph.addEdge(U, X);
 
-//     graph.addEdge(P, Q);
-//     graph.addEdge(U, V);
+    graph.addEdge(P, Q);
+    graph.addEdge(U, V);
 
-//     graph.addEdge(X, Q);
-//     graph.addEdge(X, Y);
-//     graph.addEdge(X, V);
+    graph.addEdge(X, Q);
+    graph.addEdge(X, Y);
+    graph.addEdge(X, V);
 
-//     graph.addEdge(Q, R);
-//     graph.addEdge(Y, R);
+    graph.addEdge(Q, R);
+    graph.addEdge(Y, R);
 
-//     graph.addEdge(Y, W);
-//     graph.addEdge(V, W);
+    graph.addEdge(Y, W);
+    graph.addEdge(V, W);
 
-//     graph.addEdge(R, T);
-//     graph.addEdge(W, T);
+    graph.addEdge(R, T);
+    graph.addEdge(W, T);
 
-//     var result = JSON.stringify(graph.depthFirstSearch(S));
-//     var validResult =
-//       result ===
-//         JSON.stringify(["S", "U", "V", "W", "T", "R", "Q", "Y", "X", "P"]) ||
-//       result ===
-//         JSON.stringify(["S", "P", "X", "U", "V", "W", "Y", "R", "Q", "T"]);
+    var result = JSON.stringify(graph.depthFirstSearch(S));
+    var validResult =
+      result ===
+        JSON.stringify(["S", "U", "V", "W", "T", "R", "Q", "Y", "X", "P"]) ||
+      result ===
+        JSON.stringify(["S", "P", "X", "U", "V", "W", "Y", "R", "Q", "T"]);
 
-//     expect(validResult).toBe(true);
-//   });
-// });
+    expect(validResult).toBe(true);
+  });
+});
 
 // describe("BFS", function() {
 //   it("should return an array of the nodes searched using BFS", function() {
