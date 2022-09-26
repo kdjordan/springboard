c16635995329286187ddf15db734dc21114ebca5
kdjordan <k.dean.jordan@gmail.com>
2022-09-26 10:10:53 -0700

    added graphs ex

---
+++ DSA_47/dsa-graphs/graph.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+    if (!this.nodes.has(vertex)) {
+      this.nodes.add(vertex)
+    }
+  }
+
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+    for (let node of vertexArray) {
+      if (!this.nodes.has(node)) {
+        this.nodes.add(node)
+      }
+    }
+  }
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {}
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {}
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {}
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(start) {}
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {}
+}
+
+module.exports = {Graph, Node}
