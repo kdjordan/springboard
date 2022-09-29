76db02ff074ef80f07424aae2941e6a3cfab0903
kdjordan <k.dean.jordan@gmail.com>
2022-09-28 20:53:24 -0700

    mergw passing

--- DSA_47/dsa-sorting/merge.js
+++ DSA_47/dsa-sorting/merge.js
@@ -1,5 +1,31 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let returnArr = []
+    let i = 0
+    let j = 0
 
-function mergeSort() {}
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            returnArr.push(arr1[i])
+            i++
+        } else {
+            returnArr.push(arr2[j])
+            j++
+        }
+    }
+    while (i < arr1.length) {
+        returnArr.push(arr1[i])
+        i++
+    }
+    while (j < arr2.length) {
+        returnArr.push(arr2[j])
+        j++
+    }
+    
+    return returnArr
+}
+
+function mergeSort(arr) {
+
+}
 
 module.exports = { merge, mergeSort};
