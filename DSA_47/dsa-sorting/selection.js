05a821f5737f84c805ae2c74535c8bd22c603196
kdjordan <k.dean.jordan@gmail.com>
2022-09-28 20:35:31 -0700

    selection sort working

--- DSA_47/dsa-sorting/selection.js
+++ DSA_47/dsa-sorting/selection.js
@@ -1,3 +1,19 @@
-function selectionSort() {}
+function selectionSort(arr) {    
+    for(let i = 0; i < arr.length; i++) {
+        let min = i;
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[j] < arr[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             let tmp = arr[i]; 
+             arr[i] = arr[min];
+             arr[min] = tmp;      
+        }
+    }
+    return arr;
+
+}
 
 module.exports = selectionSort;
