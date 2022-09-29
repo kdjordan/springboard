e8467bdacc76dcc39d638a413213fab1867ab830
kdjordan <k.dean.jordan@gmail.com>
2022-09-28 21:02:13 -0700

    merge sort working

--- DSA_47/dsa-sorting/merge.js
+++ DSA_47/dsa-sorting/merge.js
@@ -25,7 +25,12 @@ function merge(arr1, arr2) {
 }
 
 function mergeSort(arr) {
-
+    if (arr.length <= 1) return arr
+    let mid = Math.floor(arr.length / 2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    return merge(left, right)
+    
 }
 
 module.exports = { merge, mergeSort};
