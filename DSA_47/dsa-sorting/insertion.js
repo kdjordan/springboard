e6beadc3155ab48afd64d491fdb96b08377c902c
kdjordan <k.dean.jordan@gmail.com>
2022-09-28 20:26:06 -0700

    insertion sort tests passing

--- DSA_47/dsa-sorting/insertion.js
+++ DSA_47/dsa-sorting/insertion.js
@@ -1,3 +1,16 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for (let i=1; i < arr.length; i++) {
+        let curr = arr[i]
+
+        let j
+        
+        for (j = i - 1; j >= 0 && arr[j] > curr; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = curr
+    }
+    return arr
+}
 
 module.exports = insertionSort;
+
