05f79d83b3404a355bac9058ddd31b01bec59afb
kdjordan <k.dean.jordan@gmail.com>
2022-10-01 11:34:38 -0700

    radix sort working

--- DSA_47/dsa-sorting/radix.js
+++ DSA_47/dsa-sorting/radix.js
@@ -1,3 +1,28 @@
-function radixSort() {}
+function getDigit(num, index) {
+    return Math.floor(Math.abs(num) / Math.pow(10, index)) % 10
+}
+function digitCount(num) {
+    if (num === 0) return 1
+    return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+function mostDigits(arr) {
+    let max = 0
+    arr.map(n  => {
+        max = digitCount(n) > max ? digitCount(n) : max
+    })
+    return max
+}
+function radixSort(nums) {
+    let maxDigitCount = mostDigits(nums)
+    for (let i=0; i < maxDigitCount; i++) {
+        let digitBuckets = Array.from({length:10}, () => [])
+        for (let j=0; j < nums.length; j++) {
+            digitBuckets[getDigit(nums[j], i)].push(nums[j])
+        } 
+        nums = [].concat(...digitBuckets)
+    }
+    return nums
+}
 
-module.exports = radixSort;
+
+module.exports = { getDigit, digitCount, mostDigits, radixSort };
