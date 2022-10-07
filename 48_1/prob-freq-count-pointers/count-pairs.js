bc329efb67d4cb77fe00ca5d59fb3f683aebb49e
kdjordan <k.dean.jordan@gmail.com>
2022-10-07 13:39:44 -0700

    count pairs done

--- 48_1/prob-freq-count-pointers/count-pairs.js
+++ 48_1/prob-freq-count-pointers/count-pairs.js
@@ -1,3 +1,15 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, val) {
+    let theSet = new Set()
+    for (let i=0; i < arr.length; i++) {
+        for (k = i+1; k < arr.length; k++) {
+            console.log(arr[i], arr[k])
+            if (arr[i] + arr[k] === val) {
+                if (!theSet.has(`${arr[k]}-${arr[i]}`) || !theSet.has(`${arr[i]}-${arr[k]}`)) {
+                    theSet.add(`${arr[i]}-${arr[k]}`)
+                }
+            }
+        }
+    }
+    return theSet.size
 }
