3631982ddf0c064c650075d92f5ad02079dc59f6
kdjordan <k.dean.jordan@gmail.com>
2022-10-07 13:22:25 -0700

    same-frequency done

--- 48_1/prob-freq-count-pointers/same-frequency.js
+++ 48_1/prob-freq-count-pointers/same-frequency.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function getFreq(num) {
+    let numStr = String(num)
+    let obj = {}
+    for (let char of numStr) {
+        if (obj[char]) obj[char]++
+        else obj[char] = 1
+    }
+    return obj
 }
+function sameFrequency(num1, num2) {
+    let freq1 = getFreq(num1)
+    let freq2 = getFreq(num2)
+
+    for (let k in freq1) {
+        if (!freq2[k]) return false
+        if (freq1[k] !== freq2[k]) return false
+    }
+    return true
+}
+
+
+console.log(sameFrequency(34, 14))
