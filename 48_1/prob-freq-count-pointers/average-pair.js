83b2c8c168587e4c1af7045b895fef0d99c760c3
kdjordan <k.dean.jordan@gmail.com>
2022-10-07 11:21:28 -0700

    2 array object done

--- 48_1/prob-freq-count-pointers/average-pair.js
+++ 48_1/prob-freq-count-pointers/average-pair.js
@@ -1,4 +1,13 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr1, arr2) {
+    let obj = {}
+    for (let i=0; i < arr1.length; i++) {
+        if (arr2[i]) {
+            obj[arr1] = arr2[i]
+        } else {
+            obj[arr1] = null
+        }
+    }
 }
-    
+
+console.log(averagePair(["a", "b", "c"], [1, 2, 3]))
--- 48_1/prob-freq-count-pointers/two-array-object.js
+++ 48_1/prob-freq-count-pointers/two-array-object.js
@@ -1,3 +1,13 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+    let obj = {}
+    for (let i=0; i < arr1.length; i++) {
+        if (arr2[i]) {
+            obj[arr1[i]] = arr2[i]
+        } else {
+            obj[arr1[i]] = null
+        }
+    }
+    return obj
+    
 }
