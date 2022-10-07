cef74b6c9921223c8996fc4de4a43c01fd0f0ee8
kdjordan <k.dean.jordan@gmail.com>
2022-10-07 13:12:30 -0700

    is-subsequence done

--- 48_1/prob-freq-count-pointers/is-subsequence.js
+++ 48_1/prob-freq-count-pointers/is-subsequence.js
@@ -1,3 +1,13 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    if (str1.length === str2.length || str1.length > str2.length) return false
+    let charCount = 0
+    for (let char of str1) {
+        if (!str2.includes(char)) return false
+        else charCount++
+    }
+    if (charCount !== str1.length) return false
+    else return true
+
 }
+
