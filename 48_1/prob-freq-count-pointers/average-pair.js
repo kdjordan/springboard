cfb94e581451a978c40069b342ec3d2c2e2155b4
kdjordan <k.dean.jordan@gmail.com>
2022-10-08 07:03:17 -0700

    average pairs working

--- 48_1/prob-freq-count-pointers/average-pair.js
+++ 48_1/prob-freq-count-pointers/average-pair.js
@@ -1,13 +1,10 @@
 // add whatever parameters you deem necessary
-function averagePair(arr1, arr2) {
-    let obj = {}
-    for (let i=0; i < arr1.length; i++) {
-        if (arr2[i]) {
-            obj[arr1] = arr2[i]
-        } else {
-            obj[arr1] = null
-        }
+function averagePair(arr, val) {
+    let target = val * 2
+   for (let i=0; i < arr.length; i++) {
+    for (let k=i+1; k < arr.length; k++) {
+        if (arr[i] + arr[k] == target) return true
     }
+   }
+   return false
 }
-
-console.log(averagePair(["a", "b", "c"], [1, 2, 3]))
--- 48_1/prob-freq-count-pointers/pivot-index.js
+++ 48_1/prob-freq-count-pointers/pivot-index.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary
-function pivotIndex() {
+function returnSum(arr) {
+    let sum = 0
+    arr.forEach(el => sum += el)
+    return sum
 }
+function pivotIndex(arr) {
+    let mid = Math.floor(arr.length/2)
+    let left = 0
+    let right = arr.length
+
+    
+    while (left < right && left < arr.length && right > 0) {
+        let leftSum = returnSum(arr.slice(0, mid)) 
+        let rightSum = returnSum(arr.slice(mid+1)) 
+        if (leftSum === rightSum) return mid
+
+    }
+
+    console.log(leftSum, rightSum)
+
+}
+
+console.log(pivotIndex([5, 2, 7]))
