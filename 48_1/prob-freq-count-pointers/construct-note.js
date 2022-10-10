3c5de8b65a1673847dddc1e9bfa9b8fe221eba45
kdjordan <k.dean.jordan@gmail.com>
2022-10-09 18:59:30 -0700

    connstruct-note done

--- 48_1/prob-freq-count-pointers/construct-note.js
+++ 48_1/prob-freq-count-pointers/construct-note.js
@@ -1,3 +1,28 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function getFreq(str) {
+    let obj = {}
+    for (let i=0; i < str.length; i++) {    
+        if (obj[str[i]]) {
+            obj[str[i]] += 1
+        } else {
+            obj[str[i]] = 1
+        }
+        
+    }
+    return  obj
+}
+function constructNote(str1, str2) {
+    if (str2.length === 0) return false
+    if (str1.length === 0) return true
+    let freq1 = getFreq(str1)
+    let freq2 = getFreq(str2)
+    console.log(freq1)
+    console.log(freq2)
+    for (let i=0; i < str1.length; i++) {
+        if (!freq2[str1[i]]) return false
+        else if (!(freq2[str1[i]] >= freq1[str1[i]])) return false        
+    }
+
+    return true
+
 }
