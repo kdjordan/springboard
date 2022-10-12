02d7ccbdbf8ddc7845566698e2b8a734bee26228
kdjordan <k.dean.jordan@gmail.com>
2022-10-11 21:27:23 -0700

    curried working

--- 48_3/curried-add.js
+++ 48_3/curried-add.js
@@ -1,5 +1,11 @@
 function curriedAdd(total) {
+    if (!total) return 0
 
+    return function next(num)  {
+        if (!num) return total
+        total += num
+        return next
+    }
 }
 
 module.exports = { curriedAdd };
